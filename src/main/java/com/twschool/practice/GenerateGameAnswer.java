f3e70cc892c28b47c2d4d83875816b6ac9d02613
306093226wanggitgitgitgit <306093226@qq.com>
2020-06-16 21:05:17 +0800

    add GenerateGameAnswer.java and test

--- src/main/java/com/twschool/practice/GenerateGameAnswer.java
+++ src/main/java/com/twschool/practice/GenerateGameAnswer.java
@@ -1,4 +1,23 @@
 package com.twschool.practice;
 
-public class GenerateGameAnswer {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class GenerateGameAnswer implements GuessInputConstant {
+    public static List<Integer> GenerateNum() {
+        List<Integer> result = new ArrayList<Integer>();
+        int num = new Random().nextInt(10);
+        result.add(num);
+        for (int i = 0; i < NUMOFCHARACTER - 1; i++) {
+            while (true) {
+                num = new Random().nextInt(10);
+                if (!result.contains(num)) {
+                    result.add(num);
+                    break;
+                }
+            }
+        }
+        return result;
+    }
 }
--- src/test/java/com/twschool/practice/GuessNumberTest.java
+++ src/test/java/com/twschool/practice/GuessNumberTest.java
@@ -3,6 +3,10 @@ package com.twschool.practice;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class GuessNumberTest {
     @Test
     public void should_return_true_when_CheckInputFormat_then_input_1234(){
@@ -21,5 +25,20 @@ public class GuessNumberTest {
         String output = CheckInputFormat.checkInput(input);
         //then
         Assert.assertEquals("输入错误，再次输入",output);
+    }
+    @Test
+    public void should_return_fourNumber_when_GenerateGameAnswer(){
+        //when
+        List<Integer> output = GenerateGameAnswer.GenerateNum();
+        //then
+        Assert.assertEquals(4,output.size());
+    }
+    @Test
+    public void should_return_fourDiffNumber_when_GenerateGameAnswer(){
+        //when
+        List<Integer> output = GenerateGameAnswer.GenerateNum();
+        Set result = new HashSet(output);
+        //then
+        Assert.assertEquals(4,result.size());
     }
 }
